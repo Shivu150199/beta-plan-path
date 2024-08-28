d811457bd7297914560a6ef22aa50a81d665b3ff
shivamsingh <shivamsinghgautam397@gmail.com>
2024-08-28 17:27:11 +0530

    bg image

--- src/components/utils/Select.jsx
+++ src/components/utils/Select.jsx
@@ -6,7 +6,7 @@ import { angleDown } from '../../assets/pricing'
 const Select = ({label,placeholder,list}) => {
   return (<div className='flex flex-col gap-2 w-full'>
 <label htmlFor="" className='text-base font-medium text-dark_gray'>{label}</label>
-   <select style={{background:`url(${angleDown})`,backgroundRepeat:'no-repeat',backgroundPosition:'calc(100% - 10px) center',backgroundRepeat:'no-repeat',zIndex:'1'}} className="select  select-bordered w-full rounded-[4px] outline-none placeholder:text-placeholder">
+   <select style={{backgroundImage:`url(${angleDown})`,backgroundRepeat:'no-repeat',backgroundPosition:'calc(100% - 10px) center',backgroundRepeat:'no-repeat',backgroundSize:'10px'}} className={`select  select-bordered w-full rounded-[4px] outline-none placeholder:text-placeholder`}>
   <option disabled  className='text-placeholder'>{placeholder}</option>
   {list.map((item)=>{
     let id=nanoid()
